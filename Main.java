1d70ff701459f698e5f2c60792a751ee0e4bcc7c
ksenijavolkova84-netizen <ksenijavolkova84@gmail.com>
2025-12-11 15:49:06 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,15 @@
 package stunda2;
 
 public class Main {
-
-    public static void main(String[] args) {
-//        1	Make the cookie batter.
+    public static  void makeButter(){
+        //1	Make the cookie batter.
         System.out.println("Mix the dry ingredients.");
         System.out.println("Cream the butter and sugar.");
         System.out.println("Beat in the eggs.");
         System.out.println("Stir in the dry ingredients.");
-//        2	Bake the cookies.
+    }
+    public static void bakeCookies(){
+        //        2	Bake the cookies.
         System.out.println("Set the oven temperature.");
         System.out.println("Set the timer.");
         System.out.println("Place a batch of cookies into the oven.");
@@ -17,10 +18,15 @@ public class Main {
         System.out.println("Set the timer.");
         System.out.println("Place a batch of cookies into the oven.");
         System.out.println("Allow the cookies to bake.");
-//        3	Add frosting and sprinkles.
+    }
+    public static void decorate(){
+        //        3	Add frosting and sprinkles.
         System.out.println("Mix ingredients for frosting.");
         System.out.println("Spread frosting and sprinkles.");
 
+    }
+    public static void main(String[] args) {
+        bakeCookies();
 
     }
 }
